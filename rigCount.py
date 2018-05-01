e1bb7cea062af08dc36dc9e1b7364e3c246a0b7e
drbigbird <38020264+drbigbird@users.noreply.github.com>
2018-05-01 15:10:51 -0500

    Update of stuff

--- rigCount.py
+++ rigCount.py
@@ -12,6 +12,50 @@ url = 'http://phx.corporate-ir.net/External.File?item=UGFyZW50SUQ9NjkxMDYzfENoaW
 tmp = os.getcwd() + '\\temp.xlsb'
 fn = os.getcwd() + '\\BHGE rig data.xls'
 
+def setOptions(args):
+	"""Sets the options for what price to download
+	with the arguments:
+		comType - Commodity type
+		freq - Frequency for pricing (mm/dd/yyyy)
+		startDate - Starting date"""
+
+	comType = ''
+	freq = ''
+	startDate = None
+	dateInput = ''
+
+	# Get commodity type
+	if len(args) > 0 and args[0] in abbrDict:
+		comType = args[0]
+	else:
+		while comType not in abbrDict:
+			comType = input('Commodity (b - Brent/w - WTI/h - Henry Hub): ')
+
+	# Get frequency
+	if len(args) > 1 and args[1] in freqOpt:
+		freq = args[1]
+	else:
+		while freq not in freqOpt:
+			freq = input('Frequency (d - day/weekly - w/m - monthly/a - annual): ')
+
+	# Get start date
+	try:
+		startDate = datetime.strptime(args[2],'%m/%d/%Y')
+	except:
+		pass
+	
+	while startDate == None:
+
+		dateInput = input('Starting date (mm/dd/yyyy): ')
+		
+		try:
+			startDate = datetime.strptime(dateInput,'%m/%d/%Y')
+		except:
+			if dateInput == '':
+				startDate = datetime(1900,1,1)
+
+	return comType, freq, startDate
+
 
 def getRigFile():
 	"""Download rig count *.xlsb file and convert into *.xls
